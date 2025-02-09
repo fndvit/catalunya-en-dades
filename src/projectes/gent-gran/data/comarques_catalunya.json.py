4831c9efddbb7d2fd10279e9404727853f2ae8d5
Vicc <vmchura@gmail.com>
2025-02-09 16:58:09 +0100

    fix: :bug: Use Python tempfile to avoid OS-specific limitations in development and deployment.

--- src/projectes/gent-gran/data/comarques_catalunya.json.py
+++ src/projectes/gent-gran/data/comarques_catalunya.json.py
@@ -3,7 +3,9 @@ from zipfile import ZipFile
 from urllib.request import urlopen
 import sys
 import geopandas as gpd
+import tempfile
 
+temporal_directory = tempfile.mkdtemp()
 catalunya_zip_url = 'https://datacloud.icgc.cat/datacloud/divisions-administratives/shp/divisions-administratives-v2r1-20240705.zip'
 file_response_by_request = urlopen(catalunya_zip_url)
 zip_file_memory = ZipFile(BytesIO(file_response_by_request.read()))
@@ -12,11 +14,11 @@ zip_file_memory = ZipFile(BytesIO(file_response_by_request.read()))
 files_shape_selected = [file_name for file_name in zip_file_memory.namelist() if 'comarques-1000000' in file_name]
 for level_map_file in files_shape_selected:
     with zip_file_memory.open(level_map_file) as file:
-        with open(f'/tmp/{level_map_file}', 'wb') as f:
+        with open(f'{temporal_directory}/{level_map_file}', 'wb') as f:
             f.write(file.read())
 
 comarques_df = gpd.read_file(
-    f'/tmp/{next(file_name for file_name in files_shape_selected if file_name.endswith('.shp'))}')
+    f'{temporal_directory}/{next(file_name for file_name in files_shape_selected if file_name.endswith('.shp'))}')
 sys.stderr.writelines([str(len(comarques_df.geometry))])
 
 # Change to CRS 4326 - d3 standard
